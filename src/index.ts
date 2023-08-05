7691a8d2c8283c33f3f4041a2f581870b462c8db
Biswajeet Sutar <biswajeetsutar418@gmail.com>
2023-08-06 04:20:40 +0530

    basics of ts

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+// Write hello world
+console.log('Hello world');
+
+// Simple program
+let age: number = 20;
+
+if (age > 50)
+    age += 10;
+
+console.log(age);
+
+//
+// Debugging with tsc: build - tsconfig.json
+//
+
+// fundamentals
+let price = 123_456_789; // same as - let price: number = 123456789;
+let string = 'Typescript'; // same as - let string: string = 'Typescript';
+let bool = true; // same as - let bool: boolean = true;
+
+let level; // same as - let level: any
+
+
